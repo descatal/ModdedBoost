d5993b1df9adce938d0f2262bb86c5bb76b888d3
descatal <exvsfbmod@gmail.com>
2024-01-01 17:43:24 +0800

    - Added readme for nightly build download - Reworked some of the UI - Added translations for jp

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+# Modded Boost
 
+[![CICD](https://github.com/descatal/BoostStudio/actions/workflows/cicd.yml/badge.svg?branch=main)](https://github.com/descatal/BoostStudio/actions/workflows/cicd.yml)
+[![Chat Server](https://img.shields.io/badge/chat-discord-7289da.svg)](https://discord.gg/QAUYKmvxRN)
+
+A rpcs3 game launching / configuration UI for the Modded Boost mod.
+
+## How to run
+
+- [Download](https://nightly.link/descatal/ModdedBoost/workflows/build/main/artifacts.zip) the latest build artifact
+  and run the executable.
+
+## Credits
+
+The project is built using [tauri](https://tauri.app/) and [shadcn/ui](https://ui.shadcn.com/)
--- src/components/config.tsx
+++ src/components/config.tsx
@@ -1,12 +1,28 @@
 ﻿import {Card} from "@/components/ui/card.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {RocketIcon, UpdateIcon} from "@radix-ui/react-icons";
+import {DoubleArrowUpIcon, RocketIcon, UpdateIcon} from "@radix-ui/react-icons";
 import {Label} from "@/components/ui/label.tsx";
 import {useTranslation} from "react-i18next";
+import HoverIconButton from "@/components/common/hover-icon-button.tsx";
+import {useState} from "react";
 
 function Config({ title }: { title: string }) {
     const { t } = useTranslation()
-    
+    const [isModVersionSpinning, setModVersionSpinning] = useState(false);
+
+    const handleClick = () => {
+        // Your click handling logic here
+        console.log('Button clicked!');
+
+        setModVersionSpinning(true);
+
+        // Simulate a delay for the spinning effect
+        setTimeout(() => {
+            // Reset isSpinning after the delay
+            setModVersionSpinning(false);
+        }, 1000);
+    };
+
     return (
         <div>
             <Card className="mx-auto w-full max-w-[500px] p-6 rounded-lg shadow-lg">
@@ -17,9 +33,11 @@ function Config({ title }: { title: string }) {
                         </Label>
                         <div className="flex items-center space-x-2">
                             <span className="text-sm font-bold">1.05</span>
-                            <Button className="text-xs" variant="outline" size="icon">
-                                <UpdateIcon className="w-3 h-3"/>
-                            </Button>
+                            <HoverIconButton
+                                onClick={handleClick}
+                                tooltipContent={t("Apply updates")}
+                                icon={<DoubleArrowUpIcon className={`w-3 h-3}`} />}
+                            />
                         </div>
                     </div>
                     <div className="flex items-center justify-between">
@@ -28,9 +46,11 @@ function Config({ title }: { title: string }) {
                         </Label>
                         <div className="flex items-center space-x-2">
                             <span className="text-sm font-bold">0.0.5</span>
-                            <Button className="text-xs" variant="outline" size="icon">
-                                <UpdateIcon className="w-3 h-3"/>
-                            </Button>
+                            <HoverIconButton
+                                onClick={handleClick}
+                                tooltipContent={t("Check for updates")}
+                                icon={<UpdateIcon className={`w-3 h-3 ${isModVersionSpinning ? 'animate-spin' : ''}`} />}
+                            />
                         </div>
                     </div>
                     <Button className="w-full">
